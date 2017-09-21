4dccc8b292aae90bbc91ae87d1b7526a21c31b56
Amine195 <gmadhgs@gmail.com>
2017-09-21 15:22:08 +0100

    post request

--- public/ajax.js
+++ public/ajax.js
@@ -24,7 +24,7 @@ $('form').submit(function(e) {
     });
 }); */
 
-$('form').submit(function(e) {
+/* $('form').submit(function(e) {
     e.preventDefault();
     var formAction =$(this).attr('action');
     $.ajax({
@@ -34,4 +34,30 @@ $('form').submit(function(e) {
             console.log(data);
         }
     });
-});
+}); */
+
+$('#new-todo-form').submit(function(e) {
+    e.preventDefault();
+    
+    var toDoItem = $(this).serialize();
+
+    $.post('/todos', toDoItem, function(data) {
+        $('#todo-list').append(
+            `
+            <li class="list-group-item">
+                <span class="lead">
+                    ${data.text}
+                </span>
+                <div class="pull-right">
+                    <a href="/todos/${data._id}/edit" class="btn btn-sm btn-warning">Edit</a>
+                    <form style="display: inline" method="POST" action="/todos/${data._id}">
+                        <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                    </form>
+                </div>
+                <div class="clearfix"></div>
+            </li>
+            `
+        )
+        $('#new-todo-form').find('.form-control').val('');
+    })
+})
